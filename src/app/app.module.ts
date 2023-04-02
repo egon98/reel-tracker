7bb6615c56f093b96997537152563643a880b60a
Bognár Egon <egonbognar@gmail.com>
2023-04-02 23:33:59 +0200

    Fix Google Login

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,127 @@
-import { NgModule } from '@angular/core';
+import {CUSTOM_ELEMENTS_SCHEMA, NgModule} from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import {MatSidenavContainer, MatSidenavModule} from "@angular/material/sidenav";
+import {RouterOutlet} from "@angular/router";
+import { HeaderComponent } from './navigation/header/header.component';
+import { SidenavListComponent } from './navigation/sidenav-list/sidenav-list.component';
+import { LoginComponent } from './auth/login/login.component';
+import { SignupComponent } from './auth/signup/signup.component';
+import {AuthService} from "./auth/auth.service";
+import {
+  GoogleLoginProvider, GoogleSigninButtonModule,
+  SocialAuthService,
+  SocialAuthServiceConfig,
+  SocialLoginModule
+} from "@abacritt/angularx-social-login";
+import {MAT_DATE_LOCALE} from "@angular/material/core";
+import { AppRoutingModule } from './app-routing.module';
+import {AngularFireModule} from "@angular/fire/compat";
+import {environment} from "../environments/environment";
+import {AngularFirestoreModule} from "@angular/fire/compat/firestore";
+import {AngularFireAuthModule} from "@angular/fire/compat/auth";
+import {FlexLayoutModule} from "@angular/flex-layout";
+import {MatTabsModule} from "@angular/material/tabs";
+import {MatToolbarModule} from "@angular/material/toolbar";
+import {MatFormFieldModule} from "@angular/material/form-field";
+import {FirebaseAppModule} from "@angular/fire/app";
+import {BrowserAnimationsModule} from "@angular/platform-browser/animations";
+import {MatButtonModule} from "@angular/material/button";
+import {MatIconModule} from "@angular/material/icon";
+import { WelcomeComponent } from './welcome/welcome.component';
+import {FormsModule} from "@angular/forms";
+import {MatInputModule} from "@angular/material/input";
+import {MatDatepickerModule} from "@angular/material/datepicker";
+import {MatCheckboxModule} from "@angular/material/checkbox";
+import {MatCardModule} from "@angular/material/card";
+import {MatDividerModule} from "@angular/material/divider";
+import {MatListModule} from "@angular/material/list";
+import {MatTooltipModule} from "@angular/material/tooltip";
+import {MatTableModule} from "@angular/material/table";
+import {MatDialogModule} from "@angular/material/dialog";
+import { HomeComponent } from './home/home.component';
+import {HttpClientModule} from "@angular/common/http";
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    SidenavListComponent,
+    LoginComponent,
+    SignupComponent,
+    WelcomeComponent,
+    HomeComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    BrowserAnimationsModule,
+    FormsModule,
+    MatSidenavModule,
+    RouterOutlet,
+    AppRoutingModule,
+    FlexLayoutModule,
+    AngularFireModule.initializeApp(environment.firebaseConfig),
+    AngularFirestoreModule,
+    AngularFireAuthModule,
+    MatButtonModule,
+    MatIconModule,
+    MatToolbarModule,
+    MatInputModule,
+    MatDatepickerModule,
+    MatCheckboxModule,
+    MatCardModule,
+    MatDividerModule,
+    MatListModule,
+    MatTooltipModule,
+    MatTabsModule,
+    MatTableModule,
+    MatFormFieldModule,
+    MatDialogModule,
+    SocialLoginModule,
+    GoogleSigninButtonModule,
+    HttpClientModule
   ],
-  providers: [],
-  bootstrap: [AppComponent]
+  exports: [
+    MatSidenavModule,
+    MatTabsModule,
+    MatIconModule,
+    MatToolbarModule,
+    MatFormFieldModule,
+    FirebaseAppModule,
+    MatSidenavContainer,
+    MatButtonModule,
+    MatCheckboxModule,
+    MatCardModule,
+    MatDividerModule,
+    MatIconModule,
+    MatListModule,
+    MatTooltipModule,
+    MatTableModule,
+    MatInputModule,
+    MatDialogModule
+  ],
+  providers: [AuthService, SocialAuthService,
+    {
+      provide: 'SocialAuthServiceConfig',
+      useValue: {
+        autoLogin: false,
+        providers: [
+          {
+            id: GoogleLoginProvider.PROVIDER_ID,
+            provider: new GoogleLoginProvider(
+              '503062012461-ta5g1fprehpa3p5gea7fo73vca5mhvc1.apps.googleusercontent.com'
+            )
+          },
+        ],
+        onError: (err) => {
+          console.error(err);
+        }
+      } as SocialAuthServiceConfig,
+    },
+    {provide: MAT_DATE_LOCALE, useValue: 'en-GB' }
+  ],
+  bootstrap: [AppComponent],
+  schemas: [CUSTOM_ELEMENTS_SCHEMA]
 })
 export class AppModule { }
